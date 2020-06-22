2251b893225f452a79a11f7f88ff715ad7d823ab
Ricardo Rosa <rjrosa@outlook.com>
2020-06-22 18:45:32 +0100

    Initial Rabbit project commit.

---
+++ src/main/java/com/sparta/eng61/model/Animal.java
@@ -0,0 +1,38 @@
+package com.sparta.eng61.model;
+
+/**
+ * 
+ */
+public abstract class Animal extends World {
+
+    /**
+     * Default constructor
+     */
+    public Animal() {
+    }
+
+    /**
+     * 
+     */
+    public int age;
+
+    /**
+     * 
+     */
+    public  String gender;
+
+    /**
+     * 
+     */
+    public void lifeSpan() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+}
